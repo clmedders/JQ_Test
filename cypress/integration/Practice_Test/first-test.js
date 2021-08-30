50590c9525de622599982d670fb03660561d41cf
clmedders <christian.medders@bixal.com>
2021-08-30 16:54:38 -0400

    has example of end to end testing

--- cypress/integration/Practice_Test/first-test.js
+++ cypress/integration/Practice_Test/first-test.js
@@ -1,24 +1,53 @@
+
 describe("make sure site works", () => {
     beforeEach(() => {
         cy.visit("https://www.joshqueen.com/") 
     })
     it("page loads", () => {
-        cy.visit("https://www.joshqueen.com/") 
+        //cy.visit("https://www.joshqueen.com/") 
     })
 
-    it("contains and clicks", () =>{
-        cy.contains("About").click()
-        cy.contains("Merch").click()
-        cy.contains("Contact").click()
+    it("desktop nav should contain about link with about context", () =>{
+        //create an array of path values in the nav
+        const pathValues = [
+            '/about', '/merch', '/contact',
+        ]
+        //loop through path values 
+        //for each value find the header nav for desktop
+        //from within the desktop nav find this values link 
+        //clicking link should take me to the values page
+        pathValues.forEach(value => {
+            cy.get('.header-display-desktop').within(() => {
+                cy.get(`[href="${value}"]`, {log: true}).click()
+                cy.url().should('include', value)
+            })
+        });
+       
+        
+        //console.log(link)
+        //cy.get('[href="/merch"]')
+        //cy.get('[href="/contact"]')
+        //cy.contains('About').click()
+        //cy.contains("Merch").click()
+        //cy.contains("Contact").click()
     })
+
 
    it("goes to bottom", () => {
        cy.scrollTo("bottom")
+       cy.contains('Tour dates')
    })
+
+   it('scroll halfway', () => {
+       cy.scrollTo("center")
+   })
+
    it('contact form works', () => {
     cy.visit("https://www.joshqueen.com/contact")
-    cy.contains("Email").click("right").type("john-doe@example.com")
-    cy.contains("Message").click("right").type("This to to see if it works")
+    cy.get('[name="fname"]').type('john')
+    cy.get('[name="lname"]').type('doe')
+    cy.get('[name="email"]').type('john-doe@example.com')
+    //cy.get('[class="field-element"]').type('this is a test')
    })
 })
     
