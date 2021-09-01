e637e9063e33498df659df5e91eb7b54a0b913a5
clmedders <christian.medders@bixal.com>
2021-09-01 16:22:06 -0400

    added path loops

--- cypress/integration/Practice_Test/main_page.js
+++ cypress/integration/Practice_Test/main_page.js
@@ -4,70 +4,54 @@ describe("make sure site works", () => {
 		cy.visit("https://www.joshqueen.com/")
 
 	})
-
-	it("desktop nav should contain about link with about context", () => {
-		//create an array of path values in the nav
-		const pathValues = [
-			'/about', '/merch', '/contact',
-		]
-		//loop through path values 
-		//for each value find the header nav for desktop
-		//from within the desktop nav find this values link 
-		//clicking link should take me to the values page
-		pathValues.forEach(value => {
+	it("desktop nav should contain information links with page context", () => {
+		// const pathValues = []
+		cy.get('.header-display-desktop .header-nav-item', { log: true  }).each(($li, index) => {
+			console.log($li, index)
+			console.log($li[0].children[0].attributes[0].value)
+			const value = $li[0].children[0].attributes[0].value
 			cy.get('.header-display-desktop').within(() => {
 				cy.get(`[href="${value}"]`, { log: true }).click()
 				cy.url().should('include', value)
+			})		
+		})
+		it('header social media icons are working', () => {
+			//create path aray for heaer url  
+			const extURLs = [
+				"https://www.instagram.com/joshqueenmusic", 
+				"http://www.facebook.com/joshqueenmusic", 
+				"https://www.youtube.com/channel/UCGKkW-dYjNIrCDIv6RfaczQ",
+			]
+			//loop through extURLs stes 
+			//for each URL find the icon nav for desktop
+			//from within the desktop nav find this values link 
+			//verify that the social media icons are active 
+			extURLs.forEach(value => {
+				cy.get('.header-display-desktop .header-actions-action').within(() => {
+					//cy.get(`[href="${value}"]`, {log: true}).click()
+					//cy.url().should('include', value)
+					cy.get(`[href="${value}"]`)
+						.then(link => {
+							cy
+								.request(link.prop('href'))
+								.its('status')
+								.should('eq', 200);
+						});
+	
+				})
 			})
-		});
+		})
 
-
-		//console.log(link)
-		//cy.get('[href="/merch"]')
-		//cy.get('[href="/contact"]')
-		//cy.contains('About').click()
-		//cy.contains("Merch").click()
-		//cy.contains("Contact").click()
 	})
-
+	it('scroll halfway', () => {
+		cy.scrollTo("center")
+	})
 
 	it("Page can be scrolled to the bottom", () => {
 		//page should be able to scroll to bottom 
 		//should contain 'tour dates'
 		cy.scrollTo("bottom")
 		cy.contains('Tour dates')
-	})
-
-	it('scroll halfway', () => {
-		cy.scrollTo("center")
-	})
-
-	it('header social media icons are working', () => {
-		//create path aray for heaer url  
-		//
-		const extURLs = [
-			"https://www.instagram.com/joshqueenmusic", 
-			"http://www.facebook.com/joshqueenmusic", 
-			"https://www.youtube.com/channel/UCGKkW-dYjNIrCDIv6RfaczQ",
-		]
-		//loop through path values 
-		//for each value find the header nav for desktop
-		//from within the desktop nav find this values link 
-		//clicking link should take me to the values page
-		extURLs.forEach(value => {
-			cy.get('.header-display-desktop .header-actions-action').within(() => {
-				//cy.get(`[href="${value}"]`, {log: true}).click()
-				//cy.url().should('include', value)
-				cy.get(`[href="${value}"]`)
-					.then(link => {
-						cy
-							.request(link.prop('href'))
-							.its('status')
-							.should('eq', 200);
-					});
-
-			})
-		})
 	})
 
 })
